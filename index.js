8eb30e2b3395ac9adc90e0cf148275fac2f1768b
Topson555 <okewaleemmanuel211@gmail.com>
2026-03-04 02:43:57 -0800

    Tutorial Note

---
+++ index.js
@@ -0,0 +1,52 @@
+// console.log(`Hello`);
+// console.log(`I love you`);
+
+// window.alert(`Hello world`);
+// window.alert(`I love Ayomide and Tayo`)
+/* This is my name
+my name are
+your name */
+// document.getElementById("myh1").textContent =  `Hello world`
+// document.getElementById("myP").textContent =  `I like pizza!`
+
+//declaration let x;
+//assignment x = 200;
+
+// let x;
+// x = 250;
+// console.log(x)
+
+// let age = 50;
+// let gpa = 4.9;
+// let price = 10;
+
+// console.log(gpa);
+// console.log(price);
+// console.log(age);
+
+
+// let gpa = "bro";
+// let price = 10.8;
+// console.log(typeof price)
+// console.log(typeof gpa)
+// console.log(`You are ${age} years Old`);
+// console.log(`the price is ${price} naira`);
+// console.log(`my result is ${gpa} cgpa`);
+
+// let favouriteFood = "Yam and Egg";
+// console.log(`My favourite Food is ${favouriteFood}`)
+
+// Boolean either true or false
+// let online = true;
+// console.log(online);
+// let offline = false;
+// console.log( typeof offline);
+
+// practice
+let fullName = "Ayo, Tayo, Shade"
+let age = 22;
+let isStudent = true
+
+document.getElementById("p1").textContent = ` Your name is ${fullName}`;
+document.getElementById("p2").textContent = `you are ${age} years old`;
+document.getElementById("p3").textContent = `you are a student: ${isStudent}`;
