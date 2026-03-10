1c43f9180123cb06faa55da1f0dcdcbe207add8c
Okewale Emmanuel Tope <okewaleemmanuel211@gmailcom>
2026-03-10 04:31:38 -0700

    new bie if statement

---
+++ If statement Tuto/index.js
@@ -0,0 +1,24 @@
+let raining = true;
+if (raining){
+    console.log("Take an Unbrella");
+}
+
+let battery = 15;
+if (battery < 20){
+    console.log("charge your phone")
+}
+
+let cgp = 3;
+if (cgp < 4){
+    console.log("Second ClassUpper");
+}
+
+let age = 18;
+if (age >= 18){
+    console.log("Your are eligible to vote");
+}
+
+let password = "123456";
+if (password === "123456"){
+    console.log("Access granted")
+}
