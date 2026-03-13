b4d7b72f0cd928a4c2d9c4cf157c17761ae41d3c
Okewale Emmanuel Tope <okewaleemmanuel211@gmailcom>
2026-03-12 20:50:00 -0700

    Refactor if statements for consistency and readability

--- If statement Tuto/index.js
+++ If statement Tuto/index.js
@@ -1,24 +1,60 @@
 let raining = true;
-if (raining){
+if (raining) {
     console.log("Take an Unbrella");
 }
 
 let battery = 15;
-if (battery < 20){
+if (battery < 20) {
     console.log("charge your phone")
 }
 
 let cgp = 3;
-if (cgp < 4){
+if (cgp < 4) {
     console.log("Second ClassUpper");
 }
 
-let age = 18;
-if (age >= 18){
-    console.log("Your are eligible to vote");
-}
+// let age = 18;
+// if (age >= 18){
+//     console.log("Your are eligible to vote");
+// }
 
 let password = "123456";
-if (password === "123456"){
+if (password === "123456") {
     console.log("Access granted")
 }
+
+let Age = 13;
+if (Age >= 18) {
+    console.log("Your are eligible to vote");
+}
+else {
+    console.log("your are hold enough to vote")
+}
+
+let time = 12;
+if (time < 12) {
+    console.log("Good morning")
+}
+else {
+    console.log("Good afternoon")
+}
+
+let isStudent = true;
+if (isStudent) {
+    console.log("Your are a student")
+}
+else {
+    console.log("Your not a student")
+}
+
+let children = 20;
+if (children <= 19) {
+    console.log("Don't give birth again")
+}
+else  if(children >= 30){
+    console.log("Give more Children")
+}
+else if (children == 30){
+    console.log("Stop Child Bearing")
+}
+ 
